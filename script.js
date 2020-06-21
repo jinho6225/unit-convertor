bb77c92bc23802997104be15726071ec8b4f1f8e
jinho6225 <jinho6225@hotmail.com>
2020-06-21 01:53:14 -0700

    pregress#2

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ document.addEventListener(
   function (event) {
     // Only run on our select menu
     if (event.target.id !== 'unitConvertor') return;
-    alert(event.target.value);
   },
   false
 );
+
+let map = {
+  kg: 'lbs',
+  lbs: 'kg',
+  cm: 'inch',
+  inch: 'cm',
+};
+
+let btn = document.querySelector('.subBtn');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let unit = document.querySelector('#unitConvertor').value;
+  let number = document.querySelector('.enteredNum').value;
+  if (map[unit]) {
+    alert(map[unit]);
+  }
+
+  document.querySelector('#resultTarget').innerText = `${number}`;
+  document.querySelector('.matchingUnit').innerText = `${map[unit]}`;
+});
