68c2ab792fc527d7a937361b7e4fa82eaf3c61ff
jinho6225 <jinho6225@hotmail.com>
2020-06-21 00:57:45 -0700

    pregress#1

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // var code =
 //   'var bodyText = document.querySelector("body").innerText;alert(bodyText);';
 // chrome.tabs.executeScript({ code: code });
+
+// let btn = document.querySelector('.subBtn');
+// btn.addEventListener('click', () => {
+//   let unit = document.querySelector('#unitConvertor');
+//   document.querySelector('#resultTarget').innerText = `${number}`;
+// });
+
+document.addEventListener(
+  'input',
+  function (event) {
+    // Only run on our select menu
+    if (event.target.id !== 'unitConvertor') return;
+    alert(event.target.value);
+  },
+  false
+);
